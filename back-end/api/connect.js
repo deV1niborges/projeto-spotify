d9461aab671c65a6f0a9083513abf23679bed49e
deV1niborges <138510552+deV1niborges@users.noreply.github.com>
2025-03-02 14:02:54 -0300

    Update connect.js

--- back-end/api/connect.js
+++ back-end/api/connect.js
@@ -3,10 +3,10 @@
 // Fullfilled
 import { MongoClient } from "mongodb";
 
-const URI =
+const URL =
   "mongodb+srv://Vborges12:CGyDHdKjkPuGW1hN@cluster0.9y3hx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-const client = new MongoClient(URI);
+const client = new MongoClient(URL);
 
 export const db = client.db("spotifyAula");
 // const songCollection = await db.collection("songs").find({}).toArray();
